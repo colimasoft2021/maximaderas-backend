c3bfbfc78777c974f71e69b246ee828cabc1c87b
juanrivera-cs <juan.rivera@colimasoft.com>
2021-10-20 12:16:38 -0500

    encrypt password

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,20 +1,27 @@
 const Users = require('../models/users.model');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 async function saveUser(req, res) {
     const body = req.body;
-    try {
-      const savedUser = await Users.create(body);
-      res
-        .status(201)
-        .json(savedUser);
-    } catch (err) {
-      res
-        .status(500)
-        .json({
-          message: err
-        });
-    }
+    const passwordEncrypted = encryptPassword(req.body.password);
+    req.body.password = passwordEncrypted;
+    // try {
+    //   const savedUser = await Users.create(body);
+    //   res
+    //     .status(201)
+    //     .json(savedUser);
+    // } catch (err) {
+    //   res
+    //     .status(500)
+    //     .json({
+    //       message: err
+    //     });
+    // }
 };
-
+function encryptPassword(password){
+  const hash = bcrypt.hashSync(password, 10);
+	return hash;
+}
 module.exports = {
   saveUser
 };
